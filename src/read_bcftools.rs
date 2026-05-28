86d8269f7fcdb2f5aa40c2e1b2745bff704b7032
Tim Sengenhoff <Tim.Sengenhoff@ruhr-uni-bochum.de>
2026-05-28 12:38:42 +0200

    ignores "ENST[\d] ." input

--- src/read_bcftools.rs
+++ src/read_bcftools.rs
@@ -12,7 +12,7 @@ pub fn build_variant_dict<R: std::io::Read>(
     reader: BufReader<R>,
 ) -> Result<HashMap<u32, Vec<Variant>>> {
     let re_id = Regex::new(r"ENST\d{11}").unwrap();
-    let re_aa = Regex::new(r"\d*[A-Z]*>\d*[A-Z]*").unwrap();
+    let re_aa = Regex::new(r"\d+[A-Z]+>\d+[A-Z]+").unwrap();
 
     let mut variants: HashMap<u32, Vec<Variant>> = HashMap::new();
 
@@ -21,7 +21,9 @@ pub fn build_variant_dict<R: std::io::Read>(
 
         let (id, aa_change) = line.split_once(' ').context("Missing ' '")?;
 
-        if re_id.is_match(id)
+        //TODO handle malformed input
+        if aa_change != "."
+            && re_id.is_match(id)
             && re_aa.is_match(aa_change)
             && let Ok(key) = id[4..].parse::<u32>()
         {
@@ -34,3 +36,4 @@ pub fn build_variant_dict<R: std::io::Read>(
 
     Ok(variants)
 }
+
